b5da3533ad3bd7942ae74757eaed14f568ff1112
Graham <gwright@bso.org>
2024-04-21 17:38:42 -0400

    resolve conflict

--- index.js
+++ index.js
@@ -3,6 +3,27 @@
  * @param {Object} req Cloud Function request context.
  * @param {Object} res Cloud Function response context.
  */
-exports.helloGET = (req, res) => {
-  res.send("Hello from Cloud Functions and Cloud Source Repositories!!!");
+
+const { SecretManagerServiceClient } = require("@google-cloud/secret-manager");
+const client = new SecretManagerServiceClient();
+
+exports.helloGET = async (req, res) => {
+  const secretName =
+    "projects/fivetran-function-419419/secrets/my-secret/versions/latest";
+
+  try {
+    const [version] = await client.accessSecretVersion({
+      name: secretName,
+    });
+
+    // Extract the payload as a string.
+    const payload = version.payload.data.toString();
+
+    res
+      .status(200)
+      .send(`Hello from Cloud Functions! Secret content: ${payload}`);
+  } catch (err) {
+    console.error("Error accessing secret:", err);
+    res.status(500).send("Error accessing secret");
+  }
 };
